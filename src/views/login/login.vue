dfb179a4f29e8e988f868e53af32fb9a882bbfb4
jin.chang <jin.chagn@qiyuesuo.com>
2022-02-27 14:31:39 +0800

    feat: 添加登陆方法

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,10 +1,17 @@
 <template>
     <div class="login">
-        <h1>登陆页</h1>
+        <h1 @click="login">登陆页</h1>
     </div>
 </template>
 
 <script setup lang="ts">
+import axios from 'axios';
+
+const login = () => {
+    axios.post('http://47.103.67.54:3000/api/login', (res) => {
+        console.log(res);
+    })
+}
 </script>
 
 <style lang="less" scoped>
